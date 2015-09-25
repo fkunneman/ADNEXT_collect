483b6b121b78a9bddfc747ed8cb05aedef1463da
Florian Kunneman <f.kunneman@gmail.com>
2015-09-25 10:03:45 +0200

    fixes

--- twiqscollector.py
+++ twiqscollector.py
@@ -7,19 +7,19 @@ class Twiqscollector:
     def __init__(self, passwords, ip):
         self.pw = passwords
         self.ip = ip
-        self.requestloop = 3
-        self.requestwait = 1
+        self.requestloop = 5
+        self.requestwait = 15
         self.get_cookie()
 
     def get_cookie(self):
         session = requests.Session()
-        self.cookie = session.post('http://' + self.ip + '/cgi-bin/twitter', data = {'NAME' : self.pw[0], 'PASSWD' : self.pw[1]})
-
+        r = session.post('http://' + self.ip + '/cgi-bin/twitter', data = {'NAME' : self.pw[0], 'PASSWD' : self.pw[1]})
+        self.cookie = session.cookies
+    
     def request_tweets(self, parameters):
         try:
             retrieve = requests.get('http://' + self.ip + '/cgi-bin/twitter', params = parameters, cookies = self.cookie)
         except:
-            print('retrieve failed', parameters)
             retrieve = False
         
         return retrieve
@@ -30,16 +30,18 @@ class Twiqscollector:
         output = False
         while not output:
             output = self.request_tweets(payload)
-        print('OAUWTPOET', output)
         dumpoutput = '#user_id\t#tweet_id\t#date\t#time\t#reply_to_tweet_id\t#retweet_to_tweet_id\t#user_name\t#tweet\t#DATE=' + \
             payload['DATE'] + '\t#SEARCHTOKEN=' + keyterm + '\n'
+        print(dumpoutput)
         if output.text[:1000] == dumpoutput: #If there isn't any tweet try the request again for x times.
             for i in range(0, self.requestloop):
+                print('attempt', i)
                 output = self.request_tweets(payload)
                 while not output:
-                    print('no output')
                     time.sleep(60 * self.requestwait) #Wait for the search done at twiqs.nl before the next request
+                    output = self.request_tweets(payload)
                     if output.text != dumpoutput:
                         break
 
+        print('output', output.text)
         return output.text
