059f1677298c53b99df76f2ddcbca067904b810b
Florian Kunneman <f.kunneman@gmail.com>
2016-06-17 14:35:35 +0200

    fi

--- linewriter.py
+++ linewriter.py
@@ -25,10 +25,13 @@ class Linewriter:
             number_header[i] = header
             _cell = ws.cell(column = i, row = 1, value = header)
         for i, line in enumerate(self.lines):
+#            print('\t'.join([str(x) for x in line]).encode('utf-8'))
             i += 2
             for j, col in enumerate(line):
                 j += 1
 #                print(type(col))
+#                if j != 1:
+#                    print(i, j, col)
                 _cell = ws.cell(row = i, column = j, value = col)
                 if re.search('^http', str(col)) and not ' ' in str(col):
                     _cell.hyperlink = col
