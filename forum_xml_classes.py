8159d768c7f72c88a43b9cdb789ee919ff1c7cc1
fkunneman <f.kunneman@let.ru.nl>
2016-06-27 14:48:15 +0200

    new parse scripts

---
+++ forum_xml_classes.py
@@ -0,0 +1,71 @@
+# python json2xml_fb.py  NL_GIST_forum
+# Suzan Verberne
+import re
+
+class Forum:
+    def __init__ (self,forum_id,name,description):
+        self.forum_id = forum_id
+        self.name = name
+        self.description = description
+
+    def addThread(self,thread):
+        self.threads.append(thread)
+
+    def getNrOfThreads(self):
+        return len(self.threads)
+
+    def printXML(self,out):
+        out.write('<forum type="bb" id="'+self.forum_id+'>\n')
+        out.write('<name>'+self.name+'</name>')
+        out.write('<description>'+self.description+'</description>')
+        for thread in self.threads:
+            thread.printXML(out)
+        out.write('</forum>')
+
+class Thread:
+
+    def __init__(self,thread_id,title,category,ttype):
+        self.thread_id = thread_id
+        self.title = title
+        self.posts = []
+        self.category = category
+        self.ttype = ttype
+
+    def addPost(self,post):
+        self.posts.append(post)
+
+    def getNrOfPosts(self):
+        return len(self.posts)
+
+    def printXML(self,out):
+        self.posts = sorted(self.posts, key = lambda k : k.index)
+        out.write("<thread id=\""+self.thread_id+"\">\n<category>"+self.category+"</category>\n<title>"+self.title+"</title>\n<posts>\n")
+        for post in self.posts:
+            post.printXML(out)
+        out.write("</posts>\n</thread>\n")
+
+
+class Post:
+    postid=""
+    author=""
+    timestamp=""
+    body=""
+    parent_id=""
+
+    def __init__(self,postid,author,timestamp,body,index,parentid,ups,downs):
+        self.postid = postid
+        self.author = author
+        self.timestamp = timestamp
+        self.body = re.sub("#","&#35;",body)
+        self.body = re.sub("&","&amp;",self.body)
+        self.body = re.sub("<","&lt;",self.body)
+        self.body = re.sub(">","&gt;",self.body)
+        self.body = re.sub("=","&#61;",self.body)
+        self.body = re.sub("http[^ ]*","[URL]",self.body)
+        self.index = index
+        self.parentid = parentid
+        self.ups = ups
+        self.downs = downs
+
+    def printXML(self,out):
+        out.write("<post id=\""+self.postid+"\">\n<author>"+self.author+"</author>\n<timestamp>"+str(self.timestamp)+"</timestamp>\n<postindex>"+str(self.index)+"</postindex>\n<parentid>"+self.parentid+"</parentid>\n<body>"+self.body+"</body>\n<upvotes>"+str(self.ups)+"</upvotes>\n<downvotes>"+str(self.downs)+"</downvotes>\n</post>\n")
---
+++ parse_bodynet.py
@@ -0,0 +1,122 @@
+
+import sys
+from bs4 import BeautifulSoup
+import re
+from collections import defaultdict
+
+from forum_xml_classes import Forum, Thread, Post
+
+def parse_posthead(ph):
+
+    for c in ph.children:
+        ph_souped = BeautifulSoup(c.__str__(), 'html.parser')
+        for span in ph_souped.find_all('span'):
+            if 'class' in span.attrs:
+                if span['class'][0] == 'date':
+                    date = span.contents[0].split(',')[0]
+                elif span['class'][0] == 'time':
+                    time = span.contents[0]
+                elif span['class'][0] == 'nodecontrols':
+                    nc_souped = BeautifulSoup(span.__str__(), 'html.parser')
+                    url = nc_souped.a['href']
+                    key = nc_souped.a['name'][4:] # to maintain only the number
+                    index = int(nc_souped.a.contents[0][1:])
+    return date + ' ' + time, url, key, index
+
+def parse_postdetails(pd):
+    pd_souped = BeautifulSoup(pd.__str__(), 'html.parser')
+    for a in pd_souped.find_all('a'):
+        user = a['href'].split('/')[-1].split('.html')[0]
+    return user
+
+def parse_text(raw_text):
+    cleaned_text = re.sub(r'<[^>]+>', '', raw_text)
+    #cleaned_text = re.sub(r'\n', '', cleaned_text)
+    cleaned_text = re.sub(r' {2,}', ' ', cleaned_text)
+    return cleaned_text
+
+def parse_postbody(pb):
+
+    pb_souped = BeautifulSoup(pb.__str__(), 'html.parser')
+    for div in pb_souped.find_all('div'):
+        if 'class' in div.attrs.keys():
+            if div['class'][0] == 'content':
+                for c in div.contents:
+                    if re.search('blockquote', c.__str__()):
+                        text = parse_text(c.__str__())
+    return text
+
+def parse_thread(thr):
+
+    posts = []
+    souped = BeautifulSoup(thr, 'html.parser')
+    # for script in souped.find_all('script'):
+    #     print(script)
+    #     script_souped = BeautifulSoup(script, 'javascript.parser')
+    # for link in souped.find_all('link'):
+    #     print(link)
+    #     if 'rel' in link.attrs.keys():
+    #         if link['rel'][0] == 'alternate':
+    #             if re.search('forumid', link['href']):
+    #                 forumname = link['title']
+    #                 forumid = link['href'].split('&')[1].split('=')[1]
+    for div in souped.find_all('div'):
+        if 'class' in div.attrs.keys():
+            if div['class'][0] == 'posthead':
+                dt, url, key, index = parse_posthead(div)
+            if div['class'][0] == 'postdetails':
+                user = parse_postdetails(div)
+            elif div['class'][0] == 'postbody':
+                text = parse_postbody(div)
+                post = Post(key, user, dt, text, index, '-', '-', '-')
+                posts.append(post)
+    return posts
+
+outdir = sys.argv[1]
+forum_name = sys.argv[2]
+threads = sys.argv[3:]
+
+thread_dict = defaultdict(list)
+for thread in threads:
+    print(thread)
+    with open(thread, 'r', encoding = 'iso-8859-1') as t_open:
+        thr_str = t_open.read()
+
+    thread_details = thread.split('/')[-1].split('.')[0].split('-')
+    if re.match(r'\d+', thread_details[0]):
+        thread_id = thread_details[0]
+    else: 
+        print('No proper thread id for', thread, ', skipping')
+        continue
+
+    if re.match('print', thread_details[-1]):
+        print('page title ends with \"print\", skipping')
+        continue
+    elif re.search(r'post\d+', thread_details[-1]):
+        print('page title ends with \"post\", skipping')
+        continue
+    elif re.match(r'$\d+^', thread_details[-1]):
+        thread_index = int(thread_details[-1])
+        thread_title = ' '.join(thread_details[1:-1])
+    else:
+        thread_index = 1
+        thread_title = ' '.join(thread_details[1:])
+    thread_item = Thread(thread_id, thread_title, '-', '-')
+
+    posts = parse_thread(thr_str)
+    thread_item.posts = posts
+    thread_dict[thread_id].append(thread_item)
+
+print('Writing xml')
+for thread_id in thread_dict.keys():
+    out = open(outdir + '/threads/' + thread_id + '.xml', 'w')
+    out.write("<?xml version='1.0' encoding='iso-8859-1'?>\n")
+    out.write(r"<forum type='forum' name='" + forum_name + "'>\n")
+    thread_items = thread_dict[thread_id]
+    thread_complete = Thread(thread_id, thread_items[0].title, '-', '-')
+    for ti in thread_items:
+        for post in ti.posts:
+            thread_complete.addPost(post)
+    thread_complete.printXML(out)
+    out.write('</forum>\n')
+    out.close()
