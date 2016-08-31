e7155f8d574a590899d206d2e4e7bf266b114a06
fkunneman <f.kunneman@let.ru.nl>
2016-08-31 08:57:14 +0200

    To make it faster

--- collect_tweets_tweetids.py
+++ collect_tweets_tweetids.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env 
+###################################
+
 
 import sys
 import json
@@ -14,22 +16,27 @@ passwordfile.close()
 api = twython.Twython(passwords[0],passwords[1],passwords[2],passwords[3])
 out = open(outfile, 'a', encoding = 'utf-8')
 
-#for infile in infiles:
-#    print(infile)
 with open(infile, 'r', encoding = 'utf-8') as idfile:
     ids = idfile.read().split('\n')
 
 tweets = []
+rate = 0
 for i, tid in enumerate(ids):
+    print('Collecting tweet for id', i, 'of', len(ids))
     tweet = twitter_devs3.return_tweet(api, tid)
     if not tweet:
-        time.sleep(1801)
-        tweet = twitter_devs3.return_tweet(api, tid)
-        if not tweet:
-            print('Failure to collect this tweet two times, probably the tweet has been removed or the account has been closed')
-            continue
+        if rate > 180:
+            with open(outfile, 'a', encoding = 'utf-8') as out_w:
+                for tweet in tweets:
+                    out_w.write('\t'.join(tweet) + '\n')
+            tweets []
+            rate = 0
+            print('Rate limit probably exceeded. Now sleeping for 15 minutes.')
+            time.sleep(1800)
+            tweet = twitter_devs3.return_tweet(api, tid)
+            if not tweet:
+                print('Failure to collect this tweet two times, probably the tweet has been removed or the account has been closed.')
+                continue
+    rate += 1
     tweets.append(tweet)
 
-with open(outfile, 'w', encoding = 'utf-8') as out_w:
-    for tweet in tweets:
-        out_w.write('\t'.join(tweet) + '\n')
